4c45ea3a68c5625f57f459546b10f10b661f524a
Taylor Cool <taycool@gmail.com>
2020-05-08 09:20:11 -0700

    finished ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,23 @@
 def get_indices_of_item_weights(weights, length, limit):
+    arr = dict()
+    first = 0
+    second = 0
 
-    """
-    YOUR CODE HERE
-    """
+    if length <= 1:
+        return None
 
-    return None
+    for i in range(length):
+        arr[weights[i]] = i
+
+    for weight in arr:
+        if limit - weight in arr:
+            first = arr[weight]
+            second = arr[limit - weight]
+
+            # can't figure out how to do this
+            if limit - weight == weight:
+                first = 1
+                second = 0
+
+    print(first, second)
+    return (first, second)
