16c8b8d8cfb1d73eeb269a02f2b8dfbd79392dac
Taylor Cool <taycool@gmail.com>
2020-05-08 10:19:40 -0700

    revert push to master

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,22 +1,7 @@
 def get_indices_of_item_weights(weights, length, limit):
-    cache = {}
-    first = 0
-    second = 0
 
-    if length <= 1:
-        return None
+    """
+    YOUR CODE HERE
+    """
 
-    for i in range(length):
-        cache[weights[i]] = i
-
-    for weight in cache:
-        if limit - weight in cache:
-            first = cache[weight]
-            second = cache[limit - weight]
-
-            # can't figure out how to do this
-            if limit - weight == weight:
-                first = 1
-                second = 0
-
-    return (first, second)
+    return None
